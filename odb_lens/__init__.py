67ac94ed5a58d59ba0ff59d753104d628e32bd80
Tomsabay <104664662+Tomsabay@users.noreply.github.com>
2026-06-02 01:31:36 +0800

    feat: add odb lens recipe reports

---
+++ odb_lens/__init__.py
@@ -0,0 +1,5 @@
+"""ODB Lens recipe helpers."""
+
+from .recipe import load_recipe, normalize_recipe, render_kpi_markdown, validate_recipe
+
+__all__ = ["load_recipe", "normalize_recipe", "render_kpi_markdown", "validate_recipe"]
